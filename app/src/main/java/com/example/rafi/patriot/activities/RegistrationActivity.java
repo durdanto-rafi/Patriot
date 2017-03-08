eea5de1729af5831fe6cfecc45a354e25d90c8bc
Golam Mahmud Rafi <rafi.aust@live.com>
2017-03-08 14:53:48 +0600

    Reporting Screen

--- app/src/main/java/com/example/rafi/patriot/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/rafi/patriot/activities/RegistrationActivity.java
@@ -2,14 +2,22 @@ package com.example.rafi.patriot.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.rafi.patriot.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RegistrationActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
     }
 }
--- app/src/main/java/com/example/rafi/patriot/activities/ReportingActivity.java
+++ app/src/main/java/com/example/rafi/patriot/activities/ReportingActivity.java
@@ -2,14 +2,31 @@ package com.example.rafi.patriot.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.rafi.patriot.R;
 
-public class ReportingActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class ReportingActivity extends AppCompatActivity {
+    @BindView(R.id.spCategory)
+    Spinner spCategory;
+    @BindView(R.id.spSubCategory)
+    Spinner spSubCategory;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporting);
+        ButterKnife.bind(this);
+
+        String[] games = {"CRIME", "MOTOR VEHICLE", "LAND"};
+        Toast.makeText(this,games[0],Toast.LENGTH_SHORT).show();
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, games);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spCategory.setAdapter(arrayAdapter);
     }
 }
